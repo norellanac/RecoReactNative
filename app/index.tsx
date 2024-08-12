6e1f1dd83ad17289efd5b27d2e9d7180e3897df0
GEOVARU <alfrediviris.1@gmail.com>
2024-08-11 19:35:35 -0600

    Previeww reacNavigate

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,27 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
-import Screen1 from './screens/Screen1';
-import Screen2 from './screens/Screen2';
-import { useTranslation } from 'react-i18next';
+import { StyleSheet, Text, View,SafeAreaView } from "react-native";
+import LanguageSwitcher from "./components/molecules/LanguageSwitcher";
+import { useTranslation } from "react-i18next";
+import FormField from './components/molecules/FormField';
 import './helpers/i18n';
 import './../polyfills';
 
-export type RootStackParamList = {
-  Home: undefined;
-  Screen1: undefined;
-  Screen2: undefined;
-};
-
-const Stack = createStackNavigator<RootStackParamList>();
-
 export default function App() {
   const { t } = useTranslation();
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Screen1" component={Screen1} />
-        <Stack.Screen name="Screen2" component={Screen2} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text></Text>
+      <View style={styles.main}>
+        <Text style={styles.title}>{t('commons.start')}</Text>
+        <Text style={styles.subtitle}>{t('welcome_message')}</Text>
+        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+      <LanguageSwitcher />
+      </View>
+      <View style={styles.main}>
+        <SafeAreaView style={styles.container}>
+          <FormField />
+        </SafeAreaView>
+      </View>
+    </View>
   );
 }
 

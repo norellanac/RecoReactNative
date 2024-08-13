410f51689a1a20b171778032bfea7ffa6afe3b47
Alexis Orellana <1005alexis@gmail.com>
2024-08-13 13:07:24 -0600

    KAN-43: Implement Dark and Light Themes

--- app/index.tsx
+++ app/index.tsx
@@ -3,11 +3,14 @@ import './../polyfills';
 import 'react-native-gesture-handler';
 import RootNavigator from './routes/RootNavigator';
 import { useTranslation } from 'react-i18next';
+import { ThemeProvider } from './theme/ThemeProvider';
 
 export default function App() {
   const { t } = useTranslation();
   return ( 
-    <RootNavigator isAuthenticated={false} />
+    <ThemeProvider>
+      <RootNavigator isAuthenticated={false} />
+    </ThemeProvider>
   );
 }
 
---
+++ app/theme/themes.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+const lightTheme = {
+  colors: {
+    primary: '#6750A4',
+    secondary: '#625B71',
+    tertiary: '#7D5260',
+    background: '#FFFBFE',
+    text: '#000000',
+    // Add other color definitions here
+  },
+};
+
+const darkTheme = {
+    colors: {
+        primary: '#D0BCFF',
+        secondary: '#625B71',
+        tertiary: '#7D5260',
+        background: '#1C1B1F',
+        text: '#000000',
+        // Add other color definitions here
+    },
+};
+
+export const themes = {
+  light: lightTheme,
+  dark: darkTheme,
+};
+
+export type Theme = typeof lightTheme;

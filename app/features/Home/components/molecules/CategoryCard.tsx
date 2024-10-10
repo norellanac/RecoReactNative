34785c16494d6254eb832ec97b72e6b7ad3c45c0
Alexis Orellana <1005alexis@gmail.com>
2024-10-10 16:40:34 -0600

    feature: KAN-115 Home Screen categories component

---
+++ app/features/Home/components/molecules/CategoryCard.tsx
@@ -0,0 +1,43 @@
+import { Alert, Pressable, View } from 'react-native';
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { useTheme } from '@/app/theme/ThemeProvider';
+import { Text } from '@/app/components/atoms';
+
+type CategoryCardProps = {
+  title: string;
+  icon: React.ReactNode;
+};
+
+export default function CategoryCard({
+  title = 'CardTitle',
+  icon,
+}: CategoryCardProps) {
+  const { theme } = useTheme();
+  const { colors } = theme;
+  return (
+    <Pressable onPress={() => Alert.alert('Pressed ', title)}>
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          margin: 8,
+          width: 100,
+          height: 105,
+          borderRadius: 24,
+          backgroundColor: colors.secondary_container,
+        }}
+      >
+        {icon || <Ionicons name={'home'} size={24} color={colors.primary} />}
+        <Text
+          variant="body"
+          size="large"
+          color="primary"
+          style={{ paddingVertical: 10 }}
+        >
+          {title}
+        </Text>
+      </View>
+    </Pressable>
+  );
+}

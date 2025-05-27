d861c3a60756be2fc3f6a8ef370b78b15f2fb19a
Rolando Orellana <orellana.marketing@gmail.com>
2025-05-27 11:49:12 -0600

    Remove native and build folders from repo

--- ios/Pods/Target Support Files/ExpoAdapterFBSDKNext/ExpoAdapterFBSDKNext-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ExpoAdapterFBSDKNext : NSObject
-@end
-@implementation PodsDummy_ExpoAdapterFBSDKNext
-@end
--- ios/Pods/Target Support Files/ExpoTrackingTransparency/ExpoTrackingTransparency-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ExpoTrackingTransparency : NSObject
-@end
-@implementation PodsDummy_ExpoTrackingTransparency
-@end
